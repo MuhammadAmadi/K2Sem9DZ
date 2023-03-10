d141deabbb6dbe232f130c5cad5f48e8c2542102
Muhammad <type.92@mail.ru>
2023-03-11 01:42:44 +0300

    END

---
+++ DZ4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 61: Вывести первые N строк треугольника Паскаля. 
+// Сделать вывод в виде равнобедренного треугольника
+
+string [,] Pascal(int n)
+{
+    int value = 1;
+    string[,] pascal = new string[n, n];
+    for (int i = 1; i <= n; i++)
+    {
+        value = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            pascal[i-1, j-1] = Convert.ToString(value);
+            value = value * (i - j) / j;
+        }
+    }
+    return pascal;
+}
+
+void PrintMatrix(string[,] matrix)
+{
+    Console.Clear();
+    int line = (int)Math.Sqrt(matrix.GetLength(1))-1;
+    int left = (matrix.GetLength(1)-1)*line;
+    for (int i = 0,k=0; i < matrix.GetLength(0); i++,k+=line)
+    {
+        for (int j = 0, l = 0; j < matrix.GetLength(1); j++,l+=line)
+        {
+            if(matrix[i, j]==null) break;
+            Console.SetCursorPosition(left+l+l-k, i+1);
+            Console.Write(matrix[i, j]);
+            
+        }
+    }
+}
+Console.Write("Введите количество строк ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+PrintMatrix(Pascal(numberN)) ;
